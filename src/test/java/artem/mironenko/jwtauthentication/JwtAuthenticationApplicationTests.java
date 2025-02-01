c14012bea36d731d3c78f189f48586835d1150fb
tteemma <tttemmaa@yandex.ru>
2025-02-01 20:00:32 +0300

    Initial commit

---
+++ src/test/java/artem/mironenko/jwtauthentication/JwtAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package artem.mironenko.jwtauthentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtAuthenticationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
