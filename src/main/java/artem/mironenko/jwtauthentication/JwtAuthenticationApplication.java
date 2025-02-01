c56d0003ad9971b5a15100dbdd5523f0d7202a88
tteemma <tttemmaa@yandex.ru>
2025-02-01 20:01:37 +0300

    MainPage

--- src/main/java/artem/mironenko/jwtauthentication/JwtAuthenticationApplication.java
+++ src/main/java/artem/mironenko/jwtauthentication/JwtAuthenticationApplication.java
@@ -2,12 +2,19 @@ package artem.mironenko.jwtauthentication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class JwtAuthenticationApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(JwtAuthenticationApplication.class, args);
+    }
+    @GetMapping
+    public String hello() {
+        return "Hello World";
     }
 
 }
